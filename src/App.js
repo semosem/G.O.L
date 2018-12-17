f5be2658c658cb9bb0f3696722207cec8b9b838c
sem.gebresilassie@gmail.com <sem.gebresilassie@gmail.com>
2018-12-17 22:10:34 +0200

    compress vs decompress numbers

--- src/App.js
+++ src/App.js
@@ -10,15 +10,57 @@ class App extends PureComponent {
     this.cols = 50;
     this.state = {
       generation: 0,
+      decompressed: "whatever",
       gridFull: Array(this.rows)
         .fill()
         .map(() => Array(this.cols).fill(false))
     };
   }
+  decompress = e => {
+    const { value } = e.target;
+    const digits = value.split("");
+    this.setState({
+      decompressed: digits.filter(item => {
+        return /\d/.test(item);
+      })
+    });
+  };
   componentDidMount() {
-    console.log("seed");
     this.seed();
+    // this.playButton();
   }
+  playButton = () => {
+    this.interval = setInterval(this.play, this.speed);
+  };
+
+  play = () => {
+    let g = this.state.gridFull;
+    let g2 = g.map(item => {
+      return item;
+    });
+
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        let count = 0;
+        if (i > 0) if (g[i - 1][j]) count++;
+        if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+        if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+        if (j < this.cols - 1) if (g[i][j + 1]) count++;
+        if (j > 0) if (g[i][j - 1]) count++;
+        if (i < this.rows - 1) if (g[i + 1][j]) count++;
+        if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+        if (i < this.rows - 1 && j < this.cols - 1)
+          if (g[i + 1][j + 1]) count++;
+        if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+        if (!g[i][j] && count === 3) g2[i][j] = true;
+      }
+    }
+    this.setState({
+      gridFull: g2,
+      generation: this.state.generation + 1
+    });
+  };
+  // randomly fill some boxes
   selectBox = (row, col) => {
     const gridCopy = this.state.gridFull.map(item => {
       return item;
@@ -38,7 +80,7 @@ class App extends PureComponent {
         return Array(this.cols)
           .fill()
           .forEach((item, j) => {
-            if (Math.floor(Math.random() * 4) === 1) {
+            if (Math.floor(Math.random() * 20) === 1) {
               console.log("rand");
               gridCopy[i][j] = true;
             }
@@ -48,6 +90,7 @@ class App extends PureComponent {
       gridFull: gridCopy
     });
   };
+
   render() {
     const { gridFull } = this.state;
     return (
@@ -58,8 +101,15 @@ class App extends PureComponent {
           gridFull={gridFull}
           rows={this.rows}
           cols={this.cols}
-        />{" "}
+        />
+
         <h3>Generation: {this.state.generation}</h3>
+        <input
+          type="text"
+          className="decompressMe"
+          onChange={this.decompress}
+        />
+        <h1>Decompressed input: {this.state.decompressed}</h1>
       </div>
     );
   }
